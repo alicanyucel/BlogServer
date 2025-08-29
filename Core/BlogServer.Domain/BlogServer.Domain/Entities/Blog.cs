ec140b826b5a99344e3cf8f8975bc9f363a59c53
Ali Can Yücel <yucelalicandan@hotmail.com>
2025-08-29 18:46:42 +0300

    ...

---
+++ Core/BlogServer.Domain/BlogServer.Domain/Entities/Blog.cs
@@ -0,0 +1,13 @@
+﻿using BlogServer.Domain.Entities.Common;
+
+namespace BlogServer.Domain.Entities;
+
+public class Blog : BaseEntity
+{
+    public string Title { get; set; }
+    public string CoverImage { get; set; }
+    public string Image { get; set; }
+    public string Description { get; set; }
+    public Guid CategoryId { get; set; }
+    public Category Category { get; set; } // 1 to many relationship
+}
