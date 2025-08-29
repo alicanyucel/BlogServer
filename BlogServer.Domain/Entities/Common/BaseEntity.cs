c0c2fc6c2804d53dc602e59f137684c80e448b0f
Ali Can Yücel <yucelalicandan@hotmail.com>
2025-08-29 18:36:57 +0300

    entities

---
+++ BlogServer.Domain/Entities/Common/BaseEntity.cs
@@ -0,0 +1,8 @@
+﻿namespace BlogServer.Domain.Entities.Common;
+
+public abstract class BaseEntity
+{
+    public Guid Id { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+}
